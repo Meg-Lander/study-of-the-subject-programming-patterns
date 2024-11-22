fd99560bdd9f0e63e942566ad1d4d44944e00f05
Meg-Lander <lander.mega2003mail@mail.ru>
2024-11-22 22:34:01 +0300

    Create multiple objects and output

--- working with classes/main.rb
+++ working with classes/main.rb
@@ -1,14 +1,9 @@
-class Student
-  attr_accessor :id, :surname, :name, :middle_name, :phone, :telegram, :email, :git
+require_relative 'Student'
 
-  def initialize(surname, name, middle_name, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
-    @surname = surname
-    @name = name
-    @middle_name = middle_name
-    @id = id
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
-  end
-end
+student_ivan = Student.new('Иванов', 'Иван', 'Иванович', 1, '123456789', '@ivanov', 'ivanov@mail.com', 'github.com/ivanov')
+student_pert = Student.new('Петров', 'Пётр', 'Петрович', 2, '987654321', '@petrov', nil, 'github.com/petrov')
+student_me = Student.new('Чертоусов','Владимир','Сергеевич', 3, '987654321', nil, nil,'github.com/Meg-Lander')
+
+student_ivan.show_inf
+student_pert.show_inf
+student_me.show_inf
