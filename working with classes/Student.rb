b16cc238443fdfe2c6ac52c9b72f7239a5054344
Meg-Lander <lander.mega2003mail@mail.ru>
2024-11-22 22:03:02 +0300

    add function show_inf

--- working with classes/Student.rb
+++ working with classes/Student.rb
@@ -1,5 +1,5 @@
 class Student
-  attr_accessor :id, :surname, :name, :middle_name, :phone, :telegram, :email, :git
+
 
   def initialize(surname, name, middle_name, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
     @surname = surname
@@ -11,4 +11,33 @@ class Student
     @email = email
     @git = git
   end
+
+  #Автоматическое создание геттера и сеттера для каждого поля
+  attr_accessor :id, :surname, :name, :middle_name, :phone, :telegram, :email, :git
+
+=begin
+  это ручное создание геттера и сеттера 
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    new_name = new_name + " это имя" 
+    @name = new_name
+  end
+=end
+
+
+  def show_inf
+    puts "id: #{@id}"
+    puts "Фамилия: #{@surname}"
+    puts "Имя: #{@name}"
+    puts "Отчество: #{@middle_name}"
+    puts "Номер телефона: #{@phone}"
+    puts "Телеграм: #{@telegram}"
+    puts "Почта: #{@email}"
+    puts "GitHub: #{@git}"
+  end
+
 end
