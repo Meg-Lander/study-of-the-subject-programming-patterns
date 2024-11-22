4568f015e52f6ebbc8accb3205c64e6b6d6520f8
Meg-Lander <lander.mega2003mail@mail.ru>
2024-11-23 01:01:03 +0300

    valid? for all fields LB2.T1.8

--- working with classes/Student.rb
+++ working with classes/Student.rb
@@ -6,23 +6,56 @@ class Student
     @surname = params[:surname]
     @name = params[:name]
     @middle_name = params[:middle_name]
+
+    if params[:id] && !Student.id_valid?(params[:id])
+      raise ArgumentError, "Некорректный id: #{params[:id]}"
+    end
     @id = params[:id]
 
     if params[:phone] && !Student.phone_valid?(params[:phone])
       raise ArgumentError, "Некорректный телефонный номер: #{params[:phone]}"
     end
-
     @phone = params[:phone]
+
+    if params[:telegram] && !Student.telegram_valid?(params[:telegram])
+      raise ArgumentError, "Некорректный Telegram: #{params[:telegram]}"
+    end
     @telegram = params[:telegram]
+
+    if params[:email] && !Student.email_valid?(params[:email])
+      raise ArgumentError, "Некорректный Email: #{params[:email]}"
+    end
     @email = params[:email]
+
+    if params[:git] && !Student.git_valid?(params[:git])
+      raise ArgumentError, "Некорректный GitHub: #{params[:git]}"
+    end
     @git = params[:git]
 
+  end
+
+  def self.id_valid?(id)
+    id.to_s.match?(/^\d+$/)
   end
 
   def self.phone_valid?(phone)
     phone.to_s.match?(/^(\d{11}|\+\d{11})$/)
   end
-  
+
+  def self.telegram_valid?(telegram)
+    telegram.to_s.match?(/^@([a-zA-Z0-9_]{5,32})$/)
+  end
+
+  def self.email_valid?(email)
+    email.to_s.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  end
+
+  def self.git_valid?(git)
+    git.to_s.match?(/\Agithub\.com\/[\w-]+\z/)
+  end
+
+
+
   attr_accessor :id, :surname, :name, :middle_name, :phone, :telegram, :email, :git
 
 
