8d11375cba92940b87f32cff0495edbe36106923
Meg-Lander <lander.mega2003mail@mail.ru>
2025-01-22 01:08:06 +0300

    add 4 mathod LB4.T1.4

--- Class model/Data_list.rb
+++ Class model/Data_list.rb
@@ -10,5 +10,31 @@ class Data_list
     @elements = new_elements
   end
 
-  
+  def select(number)
+    self.elements[number]
+  end
+
+  def get_selected(ids)
+    selected_elements = []
+
+    ids.each do |id|
+      if id >= 0 && id < self.elements.size
+        selected_elements << self.elements[id]
+      end
+    end
+
+    selected_elements.join(", ")
+  end
+
+  def get_names
+    raise NotImplementedError, "Должно быть переопределено в наследнике"
+  end
+
+  def get_data
+    raise NotImplementedError, "Должно быть переопределено в наследнике"
+  end
+
+  def to_s
+    self.elements.join(", ")
+  end
 end
