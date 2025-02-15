602c36ac2d61a6354cc966b4a832a454e2d78b64
Meg-Lander <lander.mega2003mail@mail.ru>
2025-02-15 11:50:21 +0300

    Update methods in student_list LB4

--- Class model/Students_list.rb
+++ Class model/Students_list.rb
@@ -13,35 +13,36 @@ class Students_list
   def initialize(file_path, file_strategy)
     @file_path = file_path
     @file_strategy = file_strategy
+    @students = read
   end
 
-  def read_students
+  def read
     file_strategy.read(file_path)
   end
 
-  def write_students(students)
+  def write(students)
     file_strategy.write(file_path, students)
   end
 
   def find_student_by_id(id)
-    students_data = read_students
-    student_data = students_data.find { |student| student[:id] == id }
+    students_data = @students
+    student_data = students_data.find { |student| student[:id] == id.to_i }
     raise "Студент с ID #{id} не найден" unless student_data
 
-    Student.new(student_data)
+    Student.new(**student_data)
   end
 
   def get_k_n_student_short_list(k, n, data_list = nil)
-    students_data = read_students
+    students_data = @students
     total_students = students_data.size
 
     start_index = (k - 1) * n
     end_index = [start_index + n - 1, total_students - 1].min
 
-    data_list ||= DataListStudentShort.new([])
+    data_list ||= Data_list_student_short.new([])
 
     selected_students = students_data[start_index..end_index].map do |student_data|
-      StudentShort.new_obj_student_short(Student.new(student_data))
+      StudentShort.new_obj_student_short(student_data)
     end
 
     data_list.elements = selected_students
@@ -49,61 +50,56 @@ class Students_list
   end
 
   def sort_by_surname_and_initials
-    students_data = read_students
+    students_data = @students
     sorted_students = students_data.sort_by do |student|
       "#{student[:surname]} #{student[:name][0]}.#{student[:middle_name][0]}."
     end
-    write_students(sorted_students)
+    write(sorted_students)
     sorted_students
   end
 
   def add_student(new_student)
-    students_data = read_students
-    max_id = students_data.map { |student| student[:id] }.max || 0
-    new_id = max_id + 1
+    students_data = @students
 
-    new_student_data = {
-      id: new_id,
-      surname: new_student.surname,
-      name: new_student.name,
-      middle_name: new_student.middle_name,
-      git: new_student.git,
-      contact: new_student.phone || new_student.telegram || new_student.email
-    }
+    if students_data.any? { |existing| existing == new_student.to_h }
+      raise ArgumentError, "Попытка добавить дублирующегося студента: #{new_student.inspect}"
+    end
+
+    new_id = (students_data.map { |s| s[:id].to_i }.max || 0) + 1
+    new_student_data = new_student.to_h.merge(id: new_id)
 
     students_data << new_student_data
-    write_students(students_data)
+    write(students_data)
 
     new_id
   end
 
-  def replace_student_by_id(id, updated_student)
-    students_data = read_students
-    index = students_data.find_index { |student| student[:id] == id }
-    raise "Студент с ID #{id} не найден" unless index
+  def replace_student_by_id(id, new_student)
+    students_data = @students
+    index = students_data.find_index { |s| s[:id].to_s == id.to_s }
 
-    students_data[index] = {
-      id: id,
-      surname: updated_student.surname,
-      name: updated_student.name,
-      middle_name: updated_student.middle_name,
-      git: updated_student.git,
-      contact: updated_student.phone || updated_student.telegram || updated_student.email
-    }
+    if index.nil?
+      raise "Студент с ID #{id} не найден."
+    end
 
-    write_students(students_data)
+    unless students_data.any? { |existing| existing == new_student.to_h }
+      students_data[index] = new_student.to_h.merge(id: id.to_i)
+      write(students_data)
+    end
   end
+
 
   def get_student_short_count
-    read_students.size
+    @students.size
   end
 
+
   def delete_student_by_id(student_id)
-    students_data = read_students
+    students_data = @students
     student = students_data.find { |s| s[:id] == student_id }
     if student
       students_data.delete(student)
-      write_students(students_data)
+      write(students_data)
     else
       raise "Студент с ID = #{student_id} не найден"
     end
